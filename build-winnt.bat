68e6d5b0eeea9a06fdf0059586e341a7db76b561
Emily Gouge <egouge@refractions.net>
2025-05-13 16:52:09 -0700

    upgrade to v1.5.3 of esri library

--- build-winnt.bat
+++ build-winnt.bat
@@ -2,7 +2,7 @@
 set VSINSTALLDIR=C:\Program Files\Microsoft Visual Studio\2022\Community
 
 set PATH=%VSINSTALLDIR%\Common7\IDE;%PATH%
-set PATH=%VSINSTALLDIR%\VC\Tools\MSVC\14.44.32507\bin\HostX64\x64;%PATH%
+set PATH=%VSINSTALLDIR%\VC\Tools\MSVC\14.44.35207\bin\HostX64\x64;%PATH%
 
 if not exist "target\classes\natives\windows_64\" (
   md target\classes\natives\windows_64
